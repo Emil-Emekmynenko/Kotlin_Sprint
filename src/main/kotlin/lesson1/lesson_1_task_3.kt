b5ffe1af65a0a7bd04084f33cc9f30f165d71f0d
eemelyanenko <magvay7@mail.ru>
2024-01-17 23:24:10 +0300

    KS-1-3 lesson_1_task_3

---
+++ src/main/kotlin/lesson1/lesson_1_task_3.kt
@@ -0,0 +1,16 @@
+package lesson1
+
+fun main(){
+    val yearOfFirstFlightHumanInSpace = 1961
+    var hour = 9
+    var minute = 7
+
+    println("Год полёта - $yearOfFirstFlightHumanInSpace")
+    println("Час взлёта - $hour")
+    println("Минута взлёта - $minute")
+
+    hour = 10
+    minute = 55
+
+    print("Посадка произошла в $hour:$minute")
+}
