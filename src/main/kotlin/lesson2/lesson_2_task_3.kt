f88ded3a095c9d8b7e0b1a043bba1dcc7ab8db01
eemelyanenko <magvay7@mail.ru>
2024-01-19 18:59:22 +0300

    KS-2-3 lesson_2_task_3 added const

--- src/main/kotlin/lesson2/lesson_2_task_3.kt
+++ src/main/kotlin/lesson2/lesson_2_task_3.kt
@@ -1,16 +1,16 @@
 package lesson2
 
+const val constTime = 60
+
 fun main(){
     val hourDeparture = 9
     val minuteDeparture = 39
     val minuteOnWay = 457
 
-    val constTime = 60
-
     var hourArrival = minuteOnWay / constTime + hourDeparture
     var minuteArrival = (minuteOnWay % constTime) + minuteDeparture
 
-    if (minuteArrival >= 60) {
+    if (minuteArrival >= constTime) {
         hourArrival += (minuteArrival / constTime)
         minuteArrival %= constTime
     }
