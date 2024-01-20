b82341d2dec0e3464d1a08c1ac55ef4d14dc2896
eemelyanenko <magvay7@mail.ru>
2024-01-20 22:17:18 +0300

    KS-3-1 lesson_3_task_1

---
+++ src/main/kotlin/lesson3/lesson_3_task_1.kt
@@ -0,0 +1,15 @@
+package lesson3
+
+fun main() {
+    val namePlayer = "Григор"
+    val strWelcomeDay = "Доброго дня,"
+    val strWelcomeNight = "Доброго вечера,"
+    val race = "гном"
+
+    println("$strWelcomeDay $namePlayer!")
+    println("Ваша расса, $race")
+
+    println("$strWelcomeNight $namePlayer!")
+    println("Ваша расса, $race")
+
+}
