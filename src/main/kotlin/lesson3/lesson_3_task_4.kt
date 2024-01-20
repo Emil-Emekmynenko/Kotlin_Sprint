de40fe586e49cf215d0605089e2a32499c89466f
eemelyanenko <magvay7@mail.ru>
2024-01-20 23:05:14 +0300

    KS-3-4 lesson_3_task_4

---
+++ src/main/kotlin/lesson3/lesson_3_task_4.kt
@@ -0,0 +1,18 @@
+package lesson3
+
+fun main() {
+    var placeOfFigure = "E2"
+    var transitionFigure = "E4"
+    var numberMove = "1"
+
+    println("Ход белых: ")
+    println("$placeOfFigure - $transitionFigure; номер хода $numberMove")
+
+    placeOfFigure = "D2"
+    transitionFigure = "D3"
+    numberMove = "2"
+
+    println("Ход белых: ")
+    println("$placeOfFigure - $transitionFigure; номер хода $numberMove")
+
+}
