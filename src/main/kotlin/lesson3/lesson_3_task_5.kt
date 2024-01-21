31cbf3ed024eda22c21dd575b66c2bf5e95d6733
eemelyanenko <magvay7@mail.ru>
2024-01-21 12:33:10 +0300

    KS-3-5 lesson_3_task_5 added method split()

--- src/main/kotlin/lesson3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson3/lesson_3_task_5.kt
@@ -2,10 +2,12 @@ package lesson3
 
 fun main() {
     val dataMove = "D2-D4;0"
+    //преобразование строки в список
+    val listDateMove = dataMove.split('-',';')
 
-    val placeOfFigure = dataMove[0] + dataMove[1].toString()
-    val transitionFigure = dataMove[3] + dataMove[4].toString()
-    val numberMove = dataMove.substringAfter(';')
+    val placeOfFigure = listDateMove[0]
+    val transitionFigure = listDateMove[1]
+    val numberMove = listDateMove[2]
 
     println("""
         Место фигуры: $placeOfFigure
