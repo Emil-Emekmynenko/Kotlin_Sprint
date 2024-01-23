77d8f852f1c5a28eeea1cd374b544dc9449aadb0
eemelyanenko <magvay7@mail.ru>
2024-01-23 23:04:19 +0300

    KS-3-4 lesson_3_task_4 changed the variable type

--- src/main/kotlin/lesson3/lesson_3_task_4.kt
+++ src/main/kotlin/lesson3/lesson_3_task_4.kt
@@ -3,14 +3,14 @@ package lesson3
 fun main() {
     var placeOfFigure = "E2"
     var transitionFigure = "E4"
-    var numberMove = "1"
+    var numberMove = 1
 
     println("Ход белых: ")
     println("$placeOfFigure - $transitionFigure; номер хода $numberMove")
 
     placeOfFigure = "D2"
     transitionFigure = "D3"
-    numberMove = "2"
+    numberMove += 1
 
     println("Ход белых: ")
     println("$placeOfFigure - $transitionFigure; номер хода $numberMove")
