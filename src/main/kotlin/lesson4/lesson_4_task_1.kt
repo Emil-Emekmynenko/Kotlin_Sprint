2fd16140cab2dcd9852191505f9e4706dd4dfa80
eemelyanenko <magvay7@mail.ru>
2024-01-23 23:08:43 +0300

    KS-4-1 lesson_4_task_1 changed the name of the constant

--- src/main/kotlin/lesson4/lesson_4_task_1.kt
+++ src/main/kotlin/lesson4/lesson_4_task_1.kt
@@ -1,12 +1,12 @@
 package lesson4
 
-const val allTables = 13
+const val ALL_TABLES_COUNT = 13
 
 fun main() {
     val reservedTablesToday = 13
     val reservedTablesTomorrow = 9
-    val freeTablesToday = allTables - reservedTablesToday
-    val freeTablesTomorrow = allTables - reservedTablesTomorrow
+    val freeTablesToday = ALL_TABLES_COUNT - reservedTablesToday
+    val freeTablesTomorrow = ALL_TABLES_COUNT - reservedTablesTomorrow
 
     println("Доступность столиков на сегодня: ${freeTablesToday > 0},\n" +
             "Доступность столиков на завтра: ${freeTablesTomorrow > 0}.")
