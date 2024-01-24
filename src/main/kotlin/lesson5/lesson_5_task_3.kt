2d5ffeb2c7b7575f8ca465d1dd4fc2bb065e90e1
eemelyanenko <magvay7@mail.ru>
2024-01-25 00:01:58 +0300

    KS-5-3 lesson_5_task_3

---
+++ src/main/kotlin/lesson5/lesson_5_task_3.kt
@@ -0,0 +1,19 @@
+package lesson5
+
+const val FIRST_WINNING_NUMBER = 22
+const val SECOND_WINNING_NUMBER = 33
+
+fun main() {
+    println("Угадай два числа от 0 до 42 и попади на фабрику Вилли Вонки")
+    println("Введи своё первое число:")
+    val firstNumberUser = readln().toInt()
+
+    println("Введи своё второе число:")
+    val secondNumberUser = readln().toInt()
+
+    val result = if (((firstNumberUser == FIRST_WINNING_NUMBER) || (firstNumberUser == SECOND_WINNING_NUMBER)) and ((secondNumberUser == FIRST_WINNING_NUMBER) || (secondNumberUser ==SECOND_WINNING_NUMBER))) "Поздравляем! Вы выиграли главный приз!"
+    else if (((firstNumberUser == FIRST_WINNING_NUMBER) || (firstNumberUser == SECOND_WINNING_NUMBER)) || ((secondNumberUser == FIRST_WINNING_NUMBER) || (secondNumberUser ==SECOND_WINNING_NUMBER))) "Вы выиграли утешительный приз!"
+    else "Неудача!"
+
+    println(result)
+}
