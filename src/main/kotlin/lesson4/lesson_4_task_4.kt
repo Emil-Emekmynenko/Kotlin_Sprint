a66180046570de1ac4e67884c71d5fca66d25dbf
eemelyanenko <magvay7@mail.ru>
2024-01-25 21:40:02 +0300

    KS-4-4 lesson_4_task_4 corrected condition

--- src/main/kotlin/lesson4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson4/lesson_4_task_4.kt
@@ -1,25 +1,16 @@
 package lesson4
 
-const val IS_GROUP_FOR_ARM_AND_ABDOMINAL = true
-const val IS_GROUP_FOR_LEGS_AND_BACK = true
-
 fun main() {
 
     val dayOfTraining = 5
 
-    if(dayOfTraining % 2 != 0) {
-        println("""
-        Упражнения для рук: $IS_GROUP_FOR_ARM_AND_ABDOMINAL
-        Упражнения для ног: ${!IS_GROUP_FOR_LEGS_AND_BACK}
-        Упражнения для спины: ${!IS_GROUP_FOR_LEGS_AND_BACK}
-        Упражнения для пресса: $IS_GROUP_FOR_ARM_AND_ABDOMINAL""".trimIndent()
-        )
-    } else {
-        println("""
-        Упражнения для рук: ${!IS_GROUP_FOR_ARM_AND_ABDOMINAL}
-        Упражнения для ног: $IS_GROUP_FOR_LEGS_AND_BACK
-        Упражнения для спины: $IS_GROUP_FOR_LEGS_AND_BACK
-        Упражнения для пресса: ${!IS_GROUP_FOR_ARM_AND_ABDOMINAL}""".trimIndent()
-        )
-    }
+    val isEven = dayOfTraining % 2 != 0
+
+    println(
+        """
+        Упражнения для рук: $isEven
+        Упражнения для ног: ${!isEven}
+        Упражнения для спины: ${!isEven}
+        Упражнения для пресса: $isEven """.trimIndent()
+    )
 }
